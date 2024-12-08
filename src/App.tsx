eb4f3430cd8ea740e13307f403da3ab7bb2e4038
Richard Hull <rm_hull@yahoo.co.uk>
2024-12-08 14:52:03 +0000

    Enumerate devices

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    navigator.usb.getDevices().then((devices) => {
+      console.log(`Total devices: ${devices.length}`);
+      devices.forEach((device) => {
+        console.log(
+          `Product name: ${device.productName}, serial number ${device.serialNumber}`
+        );
+      });
+    });
+  }, []);
 
   return (
     <>
@@ -29,7 +40,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
